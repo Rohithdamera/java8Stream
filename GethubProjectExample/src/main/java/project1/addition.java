d1d81baa2180b4df10d6a4b2dbe6e7c0e06dc710
rohith <myselfrohith98@gmail.com>
2022-04-21 11:11:01 -0700

    Sample program

---
+++ GethubProjectExample/src/main/java/project1/addition.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class addition {
+
+	public static void main(String[] args) 
+	{
+		int a=12;
+		int b=23;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ GethubProjectExample/src/main/java/project1/subtraction.java
@@ -0,0 +1,13 @@
+package project1;
+
+public class subtraction {
+
+	public static void main(String[] args) {
+		int a=23;
+		int b=34;
+		int c=a-b;
+		System.out.println(c);
+
+	}
+
+}
